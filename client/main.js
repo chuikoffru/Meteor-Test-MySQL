dff0f2d5520d88bbf5cba7c2859641683811eaf8
chuikoff.ru <chuikoff.ru@gmail.com>
2018-02-08 10:29:23 +1200

    Add README and comments
    Refactoring...

--- client/main.js
+++ client/main.js
@@ -3,20 +3,17 @@ import { Meteor } from 'meteor/meteor';
 
 import './main.html';
 
+//Подписываемся на обновления поступающие из БД MySQL
 users = new MysqlSubscription('users');
 emails =  new MysqlSubscription('emails');
 
+//Создаем хелпер - реактивный список пользователей
 Template.hello.helpers({
-  users: () => {
-    return users.reactive();
-  }
+  users: () => users.reactive()
 });
 
+//Создаем два хелпера, реактивный список емайлов, и проверку соответствия определенного емайла пользователю
 Template.listEmail.helpers({
-  emails: () => {
-    return emails.reactive();
-  },
-  aliasExists : (email, id) => {
-    return email.hash.email.user_id == email.hash.id
-  }
+  emails: () => emails.reactive(),
+  aliasExists : (email, id) => email.hash.email.user_id == email.hash.id
 });
--- server/main.js
+++ server/main.js
@@ -1,30 +1,33 @@
-import { Meteor } from 'meteor/meteor';
-//import { console } from 'meteor/tools';
+import { Meteor } from 'meteor/meteor'
 
+//Создаем новое подключение к БД MySQL
 const liveDb = new LiveMysql({
   "host": "",
   "user": "",
   "password": "",
   "database": ""
-});
-
-Meteor.publish('emails', () => {
-  return liveDb.select(
-    'SELECT * FROM emails',
-    [ { table: 'emails' } ]
-  );
 })
 
+//Отправляем изменения в таблице emails подписчикам
+Meteor.publish('emails', () => {
+  
+  return liveDb.select(
+    'SELECT * FROM emails',
+    [
+      { table: 'emails' } 
+    ]
+  )
 
+})
+
+//Отправляем изменения в таблице users подписчикам
 Meteor.publish('users', () => {
 
   return liveDb.select(
     'SELECT * FROM users',
-    [ { table: 'users' } ]
+    [ 
+      { table: 'users' } 
+    ]
   )
 
-});
-
-Meteor.startup(() => {
-  console.log(Meteor.settings);  
-});
+})
